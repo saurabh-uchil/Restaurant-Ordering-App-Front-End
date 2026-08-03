df38e36fd27fc7e17996a6839cf36b892773b778
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-08-03 21:47:24 +1000

    Created Services for API calls

--- src/api/services/menuService.ts
+++ src/api/services/menuService.ts
@@ -4,3 +4,21 @@ export const addMenuItem = async (restaurantId: string, data: any) => {
   const response = await api.post(`/menu/${restaurantId}`, data);
   return response.data;
 };
+
+export const getMenuItems = async (restaurantId: string) => {
+  const response = await api.get(`/menu/${restaurantId}`);
+  return response.data;
+}
+
+export const editMenuItem = async (restaurantId: string, data: any) => {
+  const response = await api.put(`/menu/${restaurantId}`, data);
+  return response.data;
+};
+
+export const deleteMenuItem = async (restaurantId: string, menuItemId: string) => {
+  const response = await api.delete(`/menu/${restaurantId}/${menuItemId}`);
+  return response.data;
+};
+
+
+
