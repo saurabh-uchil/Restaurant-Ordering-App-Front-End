9aed029e35566df844a8ac412d31b8927dba6103
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-09-05 23:43:20 +1000

    Added Order Hiostory Page header

--- src/components/Kitchen/OrderHistory.tsx
+++ src/components/Kitchen/OrderHistory.tsx
@@ -1,7 +1,34 @@
+import { useParams } from "react-router";
+import KitchenHeader from "./KitchenHeader";
+import { useRestuarant } from "../../api/apihooks/useRestaurant";
+import { useSidebar } from "../../hooks/useSidebar";
+
 const OrderHistory = () => {
+
+const { restaurant } = useParams<{ restaurant: string }>();
+
+  const restaurantSlugName = restaurant ?? "";
+
+  // Get restaurant
+  const {
+    data,
+    isPending,
+    isError,
+    error,
+  } = useRestuarant(restaurantSlugName);
+
+  const {
+    isSidebarOpen,
+    toggleSidebar,
+    closeSidebar,
+  } = useSidebar();
+
   return (
     <div>
-      This is order History page
+      <KitchenHeader
+        name={data.name}
+        toggle={toggleSidebar}
+      />
     </div>
   )
 }
