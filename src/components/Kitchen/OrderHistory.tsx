d792d36c467d67b8ff677ca29d1208e9308919fc
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-09-07 00:01:39 +1000

    Added OrderHistory Side Nav and basic styling

--- src/components/Kitchen/OrderHistory.tsx
+++ src/components/Kitchen/OrderHistory.tsx
@@ -2,6 +2,9 @@ import { useParams } from "react-router";
 import KitchenHeader from "./KitchenHeader";
 import { useRestuarant } from "../../api/apihooks/useRestaurant";
 import { useSidebar } from "../../hooks/useSidebar";
+import { kitchenLinks } from "../../data/kitchenDashboardLinks";
+import SideNav from "../DashboardComponents/SideNav";
+import { styles as OrderHistoryStyles } from "../../styles/Kitchen/OrderHistory";
 
 const OrderHistory = () => {
 
@@ -24,11 +27,24 @@ const { restaurant } = useParams<{ restaurant: string }>();
   } = useSidebar();
 
   return (
-    <div>
+    <div className={OrderHistoryStyles.page}>
       <KitchenHeader
         name={data.name}
         toggle={toggleSidebar}
       />
+
+      <div className={OrderHistoryStyles.container}>
+        <SideNav
+          isOpen={isSidebarOpen}
+          onClose={closeSidebar}
+          links={kitchenLinks(restaurantSlugName)}
+          exactPath={`/restaurant/${restaurantSlugName}/kitchen`}
+        />
+
+        <div className={OrderHistoryStyles.content}>
+          <h1>Order History</h1>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/styles/Kitchen/OrderHistory.ts
@@ -0,0 +1,10 @@
+export const styles = {
+    page:
+    "min-h-screen w-full bg-[#FCFBF9] text-[#1E1B16]",
+
+  container:
+    "flex w-full",
+
+  content:
+    "min-w-0 flex-1 px-4 py-6 sm:px-6 lg:px-8",
+}
