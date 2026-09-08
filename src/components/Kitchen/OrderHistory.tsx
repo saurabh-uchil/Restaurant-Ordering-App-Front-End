3c289d2e8112420981823d414e5ec610f70b7893
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-09-08 21:55:43 +1000

    Added a sample table line

--- src/components/Kitchen/OrderHistory.tsx
+++ src/components/Kitchen/OrderHistory.tsx
@@ -44,6 +44,7 @@ const { restaurant } = useParams<{ restaurant: string }>();
         <div className={OrderHistoryStyles.content}>
           <h1>Order History</h1>
           <p>Here you can view the history of all orders placed in your restaurant.</p>
+          <p>Sample past orders in a table</p>
         </div>
       </div>
     </div>
