e11fffc0508266efd476712357df7a85fc5e09d1
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-09-09 23:24:06 +1000

    Added Sample Table

--- src/components/Kitchen/OrderHistory.tsx
+++ src/components/Kitchen/OrderHistory.tsx
@@ -45,6 +45,23 @@ const { restaurant } = useParams<{ restaurant: string }>();
           <h1>Order History</h1>
           <p>Here you can view the history of all orders placed in your restaurant.</p>
           <p>Sample past orders in a table</p>
+          <table>
+            <tr>
+              <th>Order ID</th>
+              <th>Date</th>
+              <th>Status</th>
+            </tr>
+            <tr>
+              <td>1</td>
+              <td>2023-10-01</td>
+              <td>Completed</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>2023-10-02</td>
+              <td>Pending</td>
+            </tr>
+          </table> 
         </div>
       </div>
     </div>
