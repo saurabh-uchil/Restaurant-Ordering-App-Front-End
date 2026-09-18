a5c2a710fbfb24c759ca3b4e4c84e54020133a95
Saurabh Uchil <uchilsaurabh@gmail.com>
2026-09-18 23:49:00 +1000

    Added Login page

--- src/components/Kitchen/KitchenLogin.tsx
+++ src/components/Kitchen/KitchenLogin.tsx
@@ -2,6 +2,9 @@ const KitchenLogin = () => {
   return (
     <div>
       <p>This is the kitchen login page</p>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <button>Login</button>
     </div>
   )
 }
